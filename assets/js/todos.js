2b61d5c7957af987413a4311ac22e7e49fb58f75
Felipe <fchiapini@gmail.com>
2016-02-24 15:58:06 -0300

    Added creating elements on the to-do list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check off Specific Todos By Clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 // Click on X to delete Todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text").keypress(function(event) {
+	if(event.which === 13) {
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
