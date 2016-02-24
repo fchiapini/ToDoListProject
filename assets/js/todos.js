a8e3eeecaba4a320e165ff545c5a5bc209cc9948
Felipe <fchiapini@gmail.com>
2016-02-24 18:38:38 -0300

    Styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -11,12 +11,16 @@ $("ul").on("click", "span", function(event) {
 	event.stopPropagation();
 });
 
-$("input[type='text").keypress(function(event) {
+$("input[type='text'").keypress(function(event) {
 	if(event.which === 13) {
 		//grabbing new todo text from input
 		var todoText = $(this).val();
 		$(this).val("");
 
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text'").fadeToggle();
 });
